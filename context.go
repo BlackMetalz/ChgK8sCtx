f16be8e3ea109ee9b74b6360be464643fd49ccc0
kienlt <kienlt.qn@gmail.com>
2026-01-22 11:09:16 +0700

    Add handle switch namespace

---
+++ context.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+)
+
+func switchContext(config *KubeConfig) error {
+	// Move code from main.go to here
+	// List all context and append all cluster name to a slice of string
+	// Init default slide of string
+	var contextList []string
+	// fmt.Println(strings.Repeat("=", 10) + "All context:" + strings.Repeat("=", 10))
+	for _, ctx := range config.Contexts {
+		// fmt.Println(abc.Name) // No need to print this shit.
+		// Append to slice
+		// Add a text: (current-context) next to current context to let user know this is current fucking context.
+		if ctx.Name == config.CurrentContext {
+			// We will need to remove this text when we write back to file in next step.
+			contextList = append(contextList, ctx.Name+"(current-context)")
+		} else {
+			contextList = append(contextList, ctx.Name)
+		}
+	}
+
+	// Ok, we have fucking promptui
+	prompt := promptui.Select{
+		Label: "Select k8s cluster",
+		Items: contextList,
+	}
+
+	// Run fucking prompt
+	_, result, err := prompt.Run()
+
+	// Go idiom style
+	switch err {
+	case nil:
+		// success, do nothing
+	case promptui.ErrInterrupt:
+		fmt.Println("Interrupted!")
+		return nil
+	case promptui.ErrEOF:
+		fmt.Println("Cancelled!")
+		return nil
+	default:
+		fmt.Printf("Error: %v\n", err)
+		return err
+	}
+
+	// Check if user select same context
+	if result == config.CurrentContext+"(current-context)" {
+		fmt.Println("You already selected this context!")
+		return nil
+	}
+
+	// if err != nil {
+	// 	if err == promptui.ErrInterrupt {
+	// 		// User click Ctrl+C
+	// 		fmt.Println("Interrupted by Ctrl+C! Exiting...")
+	// 	} else if err == promptui.ErrEOF {
+	// 		// User click Ctrl+D or ESC
+	// 		// Test in Mac, ESC doesn't trigger this. Only Ctrl+D works
+	// 		fmt.Println("Cancelled by Ctrl+D or ESC! Exiting...")
+	// 	} else {
+	// 		// Other errors
+	// 		fmt.Printf("Prompt failed: %v\n", err)
+	// 	}
+	// 	return
+	// }
+
+	fmt.Println("Your selection: ", result)
+
+	// Write back to file section //
+	// Remove (current-context) from result. StrimSuffix first, update struct later on.
+	result = strings.TrimSuffix(result, "(current-context)")
+	// Update struct
+	config.CurrentContext = result
+
+	// saveConfig
+	err = saveConfig("./testdata/kubeconfig", config) // config is pointer already.
+	if err != nil {
+		fmt.Println("Error writing to file")
+		return err
+	}
+
+	return nil
+}
+
+func switchNamespace(config *KubeConfig) error {
+	// Ask user to enter namespace
+	prompt := promptui.Prompt{
+		Label: "Enter namespace",
+	}
+
+	newNS, err := prompt.Run()
+	if err != nil {
+		fmt.Println("Error running promptui:", err)
+		return err
+	}
+
+	fmt.Println("Successfully switched to namespace: ", newNS)
+
+	for i, ctx := range config.Contexts {
+		if ctx.Name == config.CurrentContext { // Compare NAME
+			config.Contexts[i].Context.Namespace = newNS // Update STRUCT
+			break
+		}
+	}
+
+	// saveConfig
+	err = saveConfig("./testdata/kubeconfig", config) // config is pointer already.
+	if err != nil {
+		fmt.Println("Error writing to file")
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/manifoldco/promptui" // Promptui for interactive prompt
 	// Import this shit for unmarshal yaml
@@ -52,85 +51,22 @@ func main() {
 	switch action {
 	case "ctx":
 		fmt.Println("Change context")
+		err = switchContext(config)
+		if err != nil {
+			fmt.Println("Error running switchContext:", err)
+			return
+		}
+		return
 	case "ns":
 		fmt.Println("Change namespace")
-	default:
-		fmt.Println("Unknown action. Use 'Change context' or 'Change namespace'")
-	}
-
-	// List all context and append all cluster name to a slice of string
-	// Init default slide of string
-	var contextList []string
-	// fmt.Println(strings.Repeat("=", 10) + "All context:" + strings.Repeat("=", 10))
-	for _, ctx := range config.Contexts {
-		// fmt.Println(abc.Name) // No need to print this shit.
-		// Append to slice
-		// Add a text: (current-context) next to current context to let user know this is current fucking context.
-		if ctx.Name == config.CurrentContext {
-			// We will need to remove this text when we write back to file in next step.
-			contextList = append(contextList, ctx.Name+"(current-context)")
-		} else {
-			contextList = append(contextList, ctx.Name)
+		err = switchNamespace(config)
+		if err != nil {
+			fmt.Println("Error running switchNamespace:", err)
+			return
 		}
-	}
-
-	// Ok, we have fucking promptui
-	prompt := promptui.Select{
-		Label: "Select k8s cluster",
-		Items: contextList,
-	}
-
-	// Run fucking prompt
-	_, result, err := prompt.Run()
-
-	// Go idiom style
-	switch err {
-	case nil:
-		// success, do nothing
-	case promptui.ErrInterrupt:
-		fmt.Println("Interrupted!")
-		return
-	case promptui.ErrEOF:
-		fmt.Println("Cancelled!")
 		return
 	default:
-		fmt.Printf("Error: %v\n", err)
-		return
+		fmt.Println("Unknown action. Use 'ctx' for change context or 'ns' for change namespace")
 	}
 
-	// Check if user select same context
-	if result == config.CurrentContext+"(current-context)" {
-		fmt.Println("You already selected this context!")
-		return
-	}
-
-	// if err != nil {
-	// 	if err == promptui.ErrInterrupt {
-	// 		// User click Ctrl+C
-	// 		fmt.Println("Interrupted by Ctrl+C! Exiting...")
-	// 	} else if err == promptui.ErrEOF {
-	// 		// User click Ctrl+D or ESC
-	// 		// Test in Mac, ESC doesn't trigger this. Only Ctrl+D works
-	// 		fmt.Println("Cancelled by Ctrl+D or ESC! Exiting...")
-	// 	} else {
-	// 		// Other errors
-	// 		fmt.Printf("Prompt failed: %v\n", err)
-	// 	}
-	// 	return
-	// }
-
-	fmt.Println("Your selection: ", result)
-
-	// Write back to file section //
-	// Remove (current-context) from result. StrimSuffix first, update struct later on.
-	result = strings.TrimSuffix(result, "(current-context)")
-	// Update struct
-	config.CurrentContext = result
-
-	// saveConfig
-	err = saveConfig("./testdata/kubeconfig", config) // config is pointer already.
-	if err != nil {
-		fmt.Println("Error writing to file")
-		return
-	}
 }
